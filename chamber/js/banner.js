fa1e533e7a1117d3d4a2df541d3c047f85a5b376
jeff ward <alexward976@gmail.com>
2022-02-05 15:21:29 -0700

    Week 05 assignment

---
+++ chamber/js/banner.js
@@ -0,0 +1,10 @@
+
+let day = date.getDay();
+
+const message = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+const bannerElement = document.getElementById("event-banner");
+
+if (day === 1 || day === 2) {
+    bannerElement.textContent = message;
+    bannerElement.style.display = "block";
+}
